d4808ec9e4f6eac04d29701f62a487c537b86c82
Masiu <ciszekmarcell@gmail.com>
2019-08-05 11:25:28 +0200

    initial commit

---
+++ basics/class.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Person
+{
+private:
+  std::string firstname;
+  std::string lastname;
+  int age;
+
+public:
+  std::string getName();
+};
---
+++ basics/math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+
+  int i1 = 20;
+  int i2 = 10;
+
+  std::cout << "i1 * i2 = \n"
+            << i1 * i2 << std::endl;
+}
