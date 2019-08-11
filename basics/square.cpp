19304ec92d12da6d5d15bc0579a428fa6ce3badd
Masiu <ciszekmarcell@gmail.com>
2019-08-11 11:32:12 +0200

    c++ yoga"
    "

---
+++ basics/square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <cmath>
+
+int main()
+{
+  double x, y, z = 0;
+  x = 2;
+  y = 16;
+  z = 4;
+
+  std::cout << "Roten ur y = " << sqrt(y) << std::endl;
+  std::cout << " Upphojt z och x = " << pow(z, x) << std::endl;
+
+  system("PAUSE");
+  return 0;
+}
