cf8f73a5e0fd63fa9fcb7f1d1c6a417a60eb0daa
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-01-20 18:04:36 +0100

    Get city name

--- deno/server/main.ts
+++ deno/server/main.ts
@@ -5,10 +5,11 @@ import {config} from "https://deno.land/x/dotenv@v3.2.0/mod.ts"
 const router = new Router()
 const PORT = Deno.env.get("PORT") || 8000
 
-console.log(config())
+console.log(config().API_KEY)
+console.log(config().WEB_URL)
 
 router
-  .get("/", ctx => {
+  .get("/", (ctx) => {
     ctx.response.body = `<h1> Hello World </h1>`
   })
   // .get("/users", ctx => {
@@ -17,13 +18,13 @@ router
   //     {name: "John", age: 20},
   //     {name: "Jane", age: 21},
   //   ])
-  .get("/users", async ctx => {
+  .get("/users", async (ctx) => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users")
     const data = await res.json()
     ctx.response.type = "application/json"
     ctx.response.body = JSON.stringify(data)
   })
-  .get("/users/:id", async ctx => {
+  .get("/users/:id", async (ctx) => {
     const res = await fetch(
       `https://jsonplaceholder.typicode.com/users/${ctx.params.id}`
     )
@@ -33,8 +34,20 @@ router
     ctx.response.type = "application/json"
     ctx.response.body = JSON.stringify({data: data})
   })
-  .get("/city/:name", async ctx => {
+  .get("/city/:name", async (ctx) => {
     // TODO: Add a query param to the url
+    console.log("ctx.params", ctx.params.name)
+    const res = await fetch(
+      `${config().WEB_URL}/v1/current.json?key=${config().API_KEY}&q=${
+        ctx.params.name
+      }&aqi=no`
+    )
+    // set status code
+    if (res.ok) {
+      ctx.response.status = 200
+      const data = await res.json()
+      console.log("data", data)
+    }
   })
 
 const app = new Application()
