83f260a9e966ba62617918095a47bc0ab24ec8e6
masiucd <ciszekmarcell@gmail.com>
2023-01-28 18:36:17 +0100

    Set up Go chi router

--- go/server/go.mod
+++ go/server/go.mod
@@ -1,3 +1,8 @@
 module github.com/masiucd/just-a-proxy
 
 go 1.19
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+)
--- go/server/main.go
+++ go/server/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+  "net/http"
+
+  "github.com/go-chi/chi/middleware"
+  "github.com/go-chi/chi/v5"
+)
 
 func main() {
 
-  fmt.Println("Hello, World!")
+  r := chi.NewRouter()
+  r.Use(middleware.Logger)
+
+  r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello World"))
+  })
+
+  http.ListenAndServe(":8080", r)
 
 }
